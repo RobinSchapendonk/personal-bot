bcfa835d991e4a6cb57ba6ad5125f9cc6a9da8fa
RobinSchapendonk <ytrobinsch2@gmail.com>
2021-04-10 19:06:27 +0200

    punishments

---
+++ src/commands/punishments.js
@@ -0,0 +1,25 @@
+const { join } = require('path');
+const { punishments } = require(join(__dirname, '../utils/databases.js'));
+const { createEmbed, GetMemberFromArg } = require(join(__dirname, '../utils/message.js'));
+
+module.exports.run = async (client, message, args) => {
+	if(!message.member.hasPermission('MANAGE_MESSAGES')) return;
+	GetMemberFromArg(args[0], message.guild.members).then(async member => {
+		const amountBanned = punishments.prepare('SELECT count(uuid) FROM ban WHERE guild = ? AND member = ?').get([message.guild.id, member.id])['count(uuid)'];
+		const amountKicked = punishments.prepare('SELECT count(uuid) FROM kick WHERE guild = ? AND member = ?').get([message.guild.id, member.id])['count(uuid)'];
+		const amountWarned = punishments.prepare('SELECT count(uuid) FROM warn WHERE guild = ? AND member = ?').get([message.guild.id, member.id])['count(uuid)'];
+
+		const embed = createEmbed(`Punishments of ${member.displayName}`, '', [['Bans', amountBanned, true], ['Kicks', amountKicked, true], ['Warns', amountWarned, true]], [], 'RANDOM');
+		return message.channel.send(embed);
+	}).catch(invalidMemberError => {
+		return message.channel.send(invalidMemberError);
+	});
+};
+
+
+exports.help = {
+	name: 'punishments',
+	category: 'Moderation',
+	description: 'Check the punishments of a member',
+	usage: 'punishments <GuildMember>',
+};
